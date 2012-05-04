1d951e16612abd308ec83cecc044828da2c1d59e
Oleksandr Gituliar <gituliar@gmail.com>
2012-05-04 20:55:50 +0200

    Axiloop.m: Define Feynman rules

--- src/Axiloop/Axiloop.m
+++ src/Axiloop/Axiloop.m
@@ -9,8 +9,67 @@
 
 BeginPackage["Axiloop`", {"Axiloop`Tracer`"}]
 
+epsilon::usage =
+	"Dimensional regulator; comes from trace operations."
+
+g::usage =
+	"Quark-gluon coupling constant."
+
+n::usage =
+	"Light-cone gauge vector."
+
+FPx::usage =
+	"Crossed fermion propagator in light-cone gauge."
+	
+FP::usage =
+	"Fermion propagator in light-cone gauge."
+
+FV::usage =
+	"Fermion vertex in light-cone gauge."
+	
+GPx::usage =
+	"Crossed gluon propagator in light-cone gauge."
+	
+GP::usage =
+	"Gluon propagator in light-cone gauge."
+
+GV::usage =
+	"Gluon vertex in light-cone gauge."
+
 
 Begin["`Private`"]
+
+(* Set a space-time dimension where traces are calculated. *)
+
+VectorDimension[4-2*epsilon];
+
+(* Define Feynman rules for light-cone gauge. *)
+
+fermionLines = {};
+
+LorentzTensor[mu_, nu_, p_] :=
+	- {mu}.{nu} + (p.{mu} n.{nu} + n.{mu} p.{nu}) / p.n;
+
+FPx[p_, line_:f1] := (
+	fermionLines = Union[fermionLines, {line}];
+	I G[line, p]
+);
+
+FP[p_, line_] := FPx[p, line] / p.p;
+
+FV[mu_, line_:f1] := (
+	fermionLines = Union[fermionLines, {line}];
+	- I g G[line, {mu}]
+);
+
+GPx[mu_, nu_, p_] :=
+	I LorentzTensor[mu, nu, p];
+
+GP[mu_, nu_, p_] :=
+	GPx[mu, nu, p] / p.p;
+
+GV[i1_,p1_, i2_,p2_, i3_,p3_] :=
+	g ( {i1}.{i2} (p1.{i3}-p2.{i3}) + {i2}.{i3} (p2.{i1}-p3.{i1}) + {i3}.{i1} (p3.{i2}-p1.{i2}) );
 
 End[]
 
--- src/Tests/Axiloop.mt
+++ src/Tests/Axiloop.mt
@@ -8,8 +8,43 @@
 
 Needs["Axiloop`"]
 
+(* EquivalenceFunction for Test. It turns out that Equal doesn't work with expressions. *)
+EqualSimplify[x_, y_] := SameQ[True, Equal[Simplify[x], Simplify[y]]];
+
 Test[
 	Axiloop`Tracer`TracerVersion,
 	"1.1.1",
 	TestID->"TracerVersion"
+]
+
+Test[
+	Axiloop`Tracer`Private`d,
+	4-2*epsilon,
+	TestID->"VectorDimension"
+]
+
+Test[
+	FP[k, f1],
+	I * G[f1, k] / k.k,
+	TestID->"FermionPropagator"
+]
+
+Test[
+	FV[mu, f1],
+	- I g G[f1, {mu}],
+	TestID->"FermionVertex"
+]
+
+Test[
+	GP[mu, mu, p],
+	- I 2 (1 - epsilon) / p.p,
+	TestID->"GluonPropagator",
+	EquivalenceFunction->EqualSimplify
+]
+
+Test[
+	GV[mu,p, mu,k, nu,p],
+	g (-3 + 2 epsilon) (k.{nu} - p.{nu}),
+	TestID->"GluonVertex",
+	EquivalenceFunction->EqualSimplify
 ]
