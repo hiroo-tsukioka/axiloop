dd01dd445560643719bdd2d0f859967aa48ba3a1
Oleksandr Gituliar <gituliar@gmail.com>
2012-05-04 18:02:28 +0200

    Add Mathematica blank project

---
+++ src/Axiloop/Axiloop.m
@@ -0,0 +1,15 @@
+(* Mathematica Package *)
+
+(* Created by the Wolfram Workbench May 4, 2012 *)
+
+BeginPackage["Axiloop`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
---
+++ src/PacletInfo.m
@@ -0,0 +1,16 @@
+(* Paclet Info File *)
+
+(* created 2012/05/04*)
+
+Paclet[
+    Name -> "Axiloop",
+    Version -> "0.0.1",
+    MathematicaVersion -> "6+",
+    Creator -> "Oleksandr Gituliar",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
