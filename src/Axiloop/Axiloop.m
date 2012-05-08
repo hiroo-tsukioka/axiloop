98cb5f3e8e55a4381696518ae5b53f9da0e74a95
Oleksandr Gituliar <gituliar@gmail.com>
2012-05-08 11:26:34 +0200

    Axiloop.m: add kernel definition

--- src/Axiloop/Axiloop.m
+++ src/Axiloop/Axiloop.m
@@ -15,8 +15,17 @@ epsilon::usage =
 g::usage =
 	"Quark-gluon coupling constant."
 
+k::usage =
+	"Outgoing particle momentum; k.n = x."
+
 n::usage =
-	"Light-cone gauge vector."
+	"Light-cone gauge vector; n.n = 0."
+
+p::usage =
+	"Incoming particle momentum; p.n = 1."
+
+x::usage =
+	"x = k.n"
 
 FPx::usage =
 	"Crossed fermion propagator in light-cone gauge."
@@ -36,8 +45,20 @@ GP::usage =
 GV::usage =
 	"Gluon vertex in light-cone gauge."
 
+Kernel::usage =
+	"Defines new DGLAP evolution kernel."
 
 Begin["`Private`"]
+
+Unprotect[S];
+  S[n,n] = 0;
+  S[n,p] = 1;
+  S[p,n] = 1;
+  S[n,k] = x;
+  S[k,n] = x;
+Protect[S];
+
+Spur[f0];
 
 (* Set a space-time dimension where traces are calculated. *)
 
@@ -55,7 +76,7 @@ FPx[p_, line_:f1] := (
 	I G[line, p]
 );
 
-FP[p_, line_] := FPx[p, line] / p.p;
+FP[p_, line_:f1] := FPx[p, line] / p.p;
 
 FV[mu_, line_:f1] := (
 	fermionLines = Union[fermionLines, {line}];
@@ -70,6 +91,12 @@ GP[mu_, nu_, p_] :=
 
 GV[i1_,p1_, i2_,p2_, i3_,p3_] :=
 	g ( {i1}.{i2} (p1.{i3}-p2.{i3}) + {i2}.{i3} (p2.{i1}-p3.{i1}) + {i3}.{i1} (p3.{i2}-p1.{i2}) );
+
+(* The definition of DGLAP evolution kernel according to CFP *)
+
+Kernel[L_, M__, R_] := Module[{spurRules = (#->f0)& /@ fermionLines},
+    x ExpandNumerator[(G[f1,n]/(4 k.n))**L**NonCommutativeMultiply@@M**R //. spurRules]
+];
 
 End[]
 
--- src/Tests/Axiloop.mt
+++ src/Tests/Axiloop.mt
@@ -8,8 +8,8 @@
 
 Needs["Axiloop`"]
 
-(* EquivalenceFunction for Test. It turns out that Equal doesn't work with expressions. *)
-EqualSimplify[x_, y_] := SameQ[True, Equal[Simplify[x], Simplify[y]]];
+(* EquivalenceFunction for Test. It turns out that Equal doesn't do the job. *)
+EqualSimplify[x_, y_] := Equal[0, Simplify[x-y]];
 
 Test[
 	Axiloop`Tracer`TracerVersion,
@@ -46,5 +46,12 @@ Test[
 	GV[mu,p, mu,k, nu,p],
 	g (-3 + 2 epsilon) (k.{nu} - p.{nu}),
 	TestID->"GluonVertex",
+	EquivalenceFunction->EqualSimplify
+]
+
+Test[
+	Kernel[FP[k] ** FV[mu], { FPx[p], GPx[mu, nu, p-k]}, FV[nu] ** FP[k]],
+	2 g^2 (k.k (1 - epsilon) - 2 x k.p (1 - epsilon) + 2 x (k.k - x p.p)/(1 - x)) / (k.k)^2,
+	TestID->"LO-Kernel",
 	EquivalenceFunction->EqualSimplify
 ]
